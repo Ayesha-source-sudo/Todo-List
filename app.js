051d093e01c0f37e290f341c6a3ca874f9151454
ayesh <ayeshashoukat098@gmail.com>
2024-10-18 23:42:33 +0500

    changes in style

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ const updateTasksList = () => {
                 <p>${task.text}</p>
             </div> 
             <div class="icons">
-            <img src="./img/edit.png" onClick="editTask(${index})"/>
+            <img src="./img/edit5.png" onClick="editTask(${index})"/>
             <img src="./img/bin.png" onClick="deleteTask(${index})"/>
             </div>
         </div>
